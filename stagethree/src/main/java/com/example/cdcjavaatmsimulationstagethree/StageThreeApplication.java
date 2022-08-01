87a724341330e1338fd55b1d03aaa0371928755e
Arif Ikhsanudin <arif.ikhsanudin@mitrais.com>
2022-08-01 14:49:43 +0700

    add stage 3

---
+++ stagethree/src/main/java/com/example/cdcjavaatmsimulationstagethree/StageThreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cdcjavaatmsimulationstagethree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageThreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StageThreeApplication.class, args);
+    }
+
+}
