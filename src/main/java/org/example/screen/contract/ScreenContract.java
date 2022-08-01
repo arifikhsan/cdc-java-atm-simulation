b78fbd5b77936d246977ec3059240e2a8bb81c7c
Arif Ikhsanudin <arif.ikhsanudin@mitrais.com>
2022-08-01 13:02:48 +0700

    move to stage one

--- src/main/java/org/example/screen/contract/ScreenContract.java
+++
@@ -1,8 +0,0 @@
-package org.example.screen.contract;
-
-public abstract class ScreenContract {
-    public static ScreenContract currentScreen;
-    public static ScreenContract welcome, transaction, withdraw, otherWithdraw, summaryWithdraw, transferInputAccount, transferInputAmount, transferInputReference, transferConfirmation, transferSummary;
-
-    public void show() {}
-}
